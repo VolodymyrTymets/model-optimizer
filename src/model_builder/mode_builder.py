73f323270fd40d84a1ae587ef972ced4e897a0c0
volodymyr <volodymyrtymets@gmail.com>
2026-07-08 12:51:57 +0300

    Revert "fix gru on gpu"
    This reverts commit 6654f2150452076cd64bcbf046fd157a80c3ea7d.

--- src/model_builder/mode_builder.py
+++ src/model_builder/mode_builder.py
@@ -33,7 +33,7 @@ class ModeBuilder(IModeBuilder):
             ]
         if layer.type.value == LayerType.GRU.value:
             layers = [
-                tf.keras.layers.GRU(units=layer.units, activation=activation, return_sequences=True, unroll=True)]
+                tf.keras.layers.GRU(units=layer.units, activation=activation, return_sequences=True)]
             return layers
         else:
             raise ValueError(f"Unsupported layer type: {layer.type}")
